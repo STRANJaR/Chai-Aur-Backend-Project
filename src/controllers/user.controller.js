dce13ed37086f4d2c0c840bae1371539833f9080
Rohit Shrivastav <rohitrks805@gmail.com>
2024-01-28 13:40:05 +0530

    Add get_watch_history endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import jwt from 'jsonwebtoken'
+import mongoose from "mongoose"
 
 
 //  ***GENERATE ACCESS REFRESH TOKEN 
@@ -413,7 +414,7 @@ const getUserChannelProfile = asyncHandler( async(req, res)=>{
                     },
                     isSubscribed: {
                         $cond: {
-                            if: {$in: [req.user?._id], "$subscribers.subscriber"},
+                            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
                             then: true,
                             else: false
                         }
@@ -444,6 +445,58 @@ const getUserChannelProfile = asyncHandler( async(req, res)=>{
         
     )
 })
+
+
+// GET WATCH HISTORY 
+const getWathHistory = asyncHandler( async(req, res)=>{
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1  
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user[0].wathHistory, "watch history fetched successfully")
+    )
+})
 export {
     registerUser,
     loginUser,
@@ -454,5 +507,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWathHistory
 }
