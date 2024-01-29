503101c83f6a1f9ca3dbd3e3c48d8a1c7ce2cecc
Rohit Shrivastav <rohitrks805@gmail.com>
2024-01-30 03:03:04 +0530

    Add algorithm for upload video endpoint

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -6,7 +6,17 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 
 const uploadVideo = asyncHandler( async(req, res) => {
-    const { title, description } = req.body;
+    // Algorithm while uploading video 
+    // step 1: requirement of title, desc, and views from user (req.boyd)
+    // step 2: check for title and description !empty
+    // step 3: find video and thumbnail objec after uploading on multer middleware
+    // step 4: check for video and thumbnail objects are exists or not 
+    // step 5: upload it (video and thumbnail) on cloudinary 
+    // step 6: check video and thumbnail are uploaded 
+    // step 7: create video object in DB 
+    // step 8: return response 
+
+    const { title, description, views } = req.body;
     const  videoLocalPath  = req.files?.videoFile[0]?.path;
     const  thumbnailLocalPath = req.files?.thumbnail[0]?.path;
 
@@ -18,15 +28,15 @@ const uploadVideo = asyncHandler( async(req, res) => {
 
     if(!videoFile) throw new ApiError(400, "Faild to upload video on cloudinary")
     if(!thumbnail) throw new ApiError(400, "Faild to upload thumbnail on cloudinary")
-console.log(videoFile)
+
     const video = await Video.create(
         {
             videoFile: videoFile.url,
             thumbnail: thumbnail.url,
             title,
             description,
-            duration: null,
-            views: null,
+            duration: videoFile.duration,
+            views,
             isPublished: true,
             owner: req.user?._id
 
