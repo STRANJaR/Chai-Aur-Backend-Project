b945edf1f2fe6f5cfdabfd6e613a2ed894fbee5b
Rohit Shrivastav <rohitrks805@gmail.com>
2024-01-31 20:00:48 +0530

    Add comment functionality and add comment endpoint

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+    addComment
+
+} from '../controllers/comment.controller.js'
+const router = Router();
+
+
+router.route("/:videoId").post(verifyJWT, addComment)
+
+export default router
