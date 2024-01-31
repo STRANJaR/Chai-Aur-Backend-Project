5f12db87a67ae489f708972813ea3b97aebb3c62
Rohit Shrivastav <rohitrks805@gmail.com>
2024-01-31 20:11:32 +0530

    Add delete comment functionality and endpoint

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
-    addComment
+    addComment, deleteComment
 
 } from '../controllers/comment.controller.js'
 const router = Router();
 
 
 router.route("/:videoId").post(verifyJWT, addComment)
+router.route("/delete-comment/:commentId").delete(verifyJWT, deleteComment)
 
 export default router
