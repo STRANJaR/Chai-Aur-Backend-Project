66f048dc3a1e3e22be71981a4bfe9f80da532b86
Rohit Shrivastav <rohitrks805@gmail.com>
2024-03-08 00:21:26 +0530

    react-router-dom | Home initialize

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Login() {
+    console.log("Login render");
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import './index.css'
+import Home from './components/Home.jsx'
+import Login from './components/Login.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+    children: [
+      {
+        path: 'login',
+        element: <Login/>
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}>
+
     <App />
+    </RouterProvider>
   </React.StrictMode>,
 )
