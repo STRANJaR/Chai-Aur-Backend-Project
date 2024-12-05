8eb00295af601d9bb38acbda275cc5b7e7fbf041
Rohit Shrivastav <rohitrks805@gmail.com>
2024-12-06 01:33:02 +0530

    implemented update avatar and cover image api

--- frontend/src/components/User/UpdateAccountDetails.jsx
+++ frontend/src/components/User/UpdateAccountDetails.jsx
@@ -5,9 +5,10 @@ import { Button } from '../ui/button'
 import { Loader2, PencilLine } from 'lucide-react'
 import { Separator } from '../ui/separator'
 import FileUpload from '../FileUpload'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import axios from 'axios'
 import { toast, ToastContainer } from 'react-toastify'
+import { setCredentials } from '../../store/authSlice'
 
 
 const UpdateAccountDetails = () => {
@@ -16,6 +17,10 @@ const UpdateAccountDetails = () => {
     const token = useSelector(state => state.auth.token);
 
     const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const [avatarFile, setAvatarFile] = useState(null);
+    const [coverImage, setCoverImage] = useState(null);
+
 
 
     const [personalLoading, setIsPersonalLoading] = useState(false)
@@ -28,10 +33,12 @@ const UpdateAccountDetails = () => {
         console.log('payload: ', payload)
         setIsPersonalLoading(true);
 
+
+        // TODO: update single field as per user req. 
         const updateData = {}
 
-        if(payload.name) updateData.name = payload.name;
-        if(payload.email) updateData.email = payload.email;
+        if (payload.name) updateData.name = payload.name;
+        if (payload.email) updateData.email = payload.email;
 
         try {
             const response = await axios.patch('http://localhost:8000/api/v1/users/update-account-details',
@@ -59,138 +66,205 @@ const UpdateAccountDetails = () => {
     }
 
 
-    const handleAvatarChange = () => {
+    // Update profile picture
+    const handleAvatarChange = async () => {
 
+
+        const formData = new FormData();
+        if (avatarFile) formData.append('avatarLocalPath', avatarFile)
+
+        setIsAvatarLoading(true);
+        try {
+            const response = await axios.patch('http://localhost:8000/api/v1/users/update-avatar',
+                {
+                    avatar: formData.get('avatarLocalPath')
+                },
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                        "Authorization": `Bearer ${token}`
+                    }
+                }
+            );
+
+            console.log(response)
+            toast.success(response.data.message)
+
+            // save fresh user object to local storage
+            localStorage.setItem('user', JSON.stringify(response.data.data))
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.response.data.data)
+            setIsAvatarLoading(false)
+        }
+
+        setIsAvatarLoading(false)
     }
 
-    const handleCoverImageChange = () => {
 
+    // Update cover image 
+    const handleCoverImageChange = async () => {
+        setCoverImageLoading(true);
+
+        const formData = new FormData();
+        if (coverImage) formData.append('coverImage', coverImage)
+
+        try {
+            const response = await axios.patch('http://localhost:8000/api/v1/users/update-cover-image',
+                {
+                    coverImage: coverImage
+                },
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                        "Authorization": `Bearer ${token}`
+                    }
+                }
+            );
+
+            console.log(response)
+            toast.success(response.data.message)
+
+
+            // save fresh user object to local storage
+            localStorage.setItem('user', JSON.stringify(response.data.data))
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.response.data.data)
+            setCoverImageLoading(false)
+        }
+
+        setCoverImageLoading(false)
     }
 
 
     return (
         <>
-        <section className='h-screen w-full text-xs'>
-            <div className='grid grid-cols-3'>
-                <div className='h-screen w-full col-span-1 border-r-2'>
+            <section className='h-screen w-full text-xs'>
+                <div className='grid grid-cols-3'>
+                    <div className='h-screen w-full col-span-1 border-r-2'>
 
-                </div>
-                {/* <Separator/> */}
+                    </div>
+                    {/* <Separator/> */}
 
-                <div className='h-screen w-full col-span-2 '>
+                    <div className='h-screen w-full col-span-2 '>
 
-                    <main className='h-auto w-[70%] relative top-10 left-32'>
+                        <main className='h-auto w-[70%] relative top-10 left-32'>
 
-                        {/* UPDATE SECTION FOR: NAME AND EMAIL  */}
-                        <section className='h-auto w-full py-5'>
-                            <form onSubmit={handleSubmit(personalDetailChange)}>
-                                <div className='flex flex-col justify-center  gap-3'>
-                                    <div className='flex flex-row items-center gap-2'>
-                                        <PencilLine className='h-4 w-4' />
-                                        <h4 className='font-semibold text-gray-700 dark:text-gray-300'>PERSONAL DETAILS</h4>
+                            {/* FEATURE: UPDATE SECTION FOR: NAME AND EMAIL  */}
+                            <section className='h-auto w-full py-5'>
+                                <form onSubmit={handleSubmit(personalDetailChange)}>
+                                    <div className='flex flex-col justify-center  gap-3'>
+                                        <div className='flex flex-row items-center gap-2'>
+                                            <PencilLine className='h-4 w-4' />
+                                            <h4 className='font-semibold text-gray-700 dark:text-gray-300'>PERSONAL DETAILS</h4>
+                                        </div>
+                                        <Input
+                                            className='outline-none '
+                                            type='text'
+                                            defaultValue={user.fullName}
+                                            placeholder='Enter your name'
+                                            {...register('name')}
+                                        />
+                                        {errors.name && <p className='text-red-500'>This field is required*</p>}
+
+                                        <Input
+                                            className='outline-none'
+                                            type='email'
+                                            defaultValue={user.email}
+                                            placeholder='Enter your email'
+                                            {...register('email')}
+                                        />
+                                        {errors.email && <p className='text-red-500'>This field is required*</p>}
+
+                                        <Button
+                                            className='w-full'
+                                            type='submit'>
+                                            {
+                                                personalLoading ?
+                                                    <Loader2 className='h-4 w-4 animate-spin' />
+                                                    :
+                                                    'Update'
+                                            }
+                                        </Button>
                                     </div>
-                                    <Input
-                                        className='outline-none '
-                                        type='text'
-                                        defaultValue={user.fullName}
-                                        placeholder='Enter your name'
-                                        {...register('name')}
-                                    />
-                                    {errors.name && <p className='text-red-500'>This field is required*</p>}
+                                </form>
+                            </section>
 
-                                    <Input
-                                        className='outline-none'
-                                        type='email'
-                                        defaultValue={user.email}
-                                        placeholder='Enter your email'
-                                        {...register('email')}
-                                    />
-                                    {errors.email && <p className='text-red-500'>This field is required*</p>}
+                            <Separator />
 
-                                    <Button
-                                        className='w-full'
-                                        type='submit'>
-                                        {
-                                            personalLoading ?
-                                                <Loader2 className='h-4 w-4 animate-spin' />
-                                                :
-                                                'Update'
-                                        }
-                                    </Button>
-                                </div>
-                            </form>
-                        </section>
-
-                        <Separator />
-
-                        {/* UPDATE SECTION FOR: AVATAR  */}
-                        <section className='h-auto w-full py-5'>
-                            <form onSubmit={handleSubmit(handleAvatarChange)}>
-                                <div className='flex flex-col justify-center  gap-3'>
-                                    <div className='flex flex-row items-center gap-2'>
-                                        <PencilLine className='h-4 w-4' />
-                                        <h4 className='font-semibold text-gray-700 dark:text-gray-300'>PROFILE PICTURE</h4>
-                                    </div>
-                                    {/* <Input
+                            {/* FEATURE: UPDATE SECTION FOR: AVATAR  */}
+                            <section className='h-auto w-full py-5'>
+                                <form onSubmit={handleSubmit(handleAvatarChange)}>
+                                    <div className='flex flex-col justify-center  gap-3'>
+                                        <div className='flex flex-row items-center gap-2'>
+                                            <PencilLine className='h-4 w-4' />
+                                            <h4 className='font-semibold text-gray-700 dark:text-gray-300'>PROFILE PICTURE</h4>
+                                        </div>
+                                        {/* <Input
                                         className='outline-none '
                                         type='file'
-                                        {...register('avatar', { required: true })}
+                                        {...register('avatar', { required: true })} 
                                     />
                                     {errors.name && <p className='text-red-500'>error.name.message</p>} */}
 
-                                    <FileUpload handleChange={handleAvatarChange} />
-                                    <Button
-                                        className='w-full'
-                                        type='submit'>
-                                        {
-                                            avatarLoading ?
-                                                <Loader2 className='h-4 w-4 animate-spin' />
-                                                :
-                                                'Update'
-                                        }
-                                    </Button>
-                                </div>
-                            </form>
-                        </section>
+                                        <FileUpload
 
-
-
-                        <Separator />
-
-                        {/* UPDATE SECTION FOR: COVER IMAGE  */}
-                        <section className='h-auto w-full py-5'>
-                            <form onSubmit={handleSubmit(handleCoverImageChange)}>
-                                <div className='flex flex-col justify-center  gap-3'>
-
-                                    <div className='flex flex-row items-center gap-2'>
-                                        <PencilLine className='h-4 w-4' />
-                                        <h4 className='font-semibold text-gray-700 dark:text-gray-300'>COVER IMAGE</h4>
+                                            handleChange={e => setCoverImage(e.target.files[0])} />
+                                        <Button
+                                            className='w-full'
+                                            type='submit'>
+                                            {
+                                                avatarLoading ?
+                                                    <Loader2 className='h-4 w-4 animate-spin' />
+                                                    :
+                                                    'Update'
+                                            }
+                                        </Button>
                                     </div>
+                                </form>
+                            </section>
 
-                                    <FileUpload
-                                        handleChange={handleAvatarChange} />
-                                    <Button
-                                        className='w-full'
-                                        type='submit'>
-                                        {
-                                            coverImageLoading ?
-                                                <Loader2 className='h-4 w-4 animate-spin' />
-                                                :
-                                                'Update'
-                                        }
-                                    </Button>
-                                </div>
-                            </form>
-                        </section>
-                    </main>
+
+
+                            <Separator />
+
+                            {/* FEATURE: UPDATE SECTION FOR: COVER IMAGE  */}
+                            <section className='h-auto w-full py-5'>
+                                <form onSubmit={handleSubmit(handleCoverImageChange)}>
+                                    <div className='flex flex-col justify-center  gap-3'>
+
+                                        <div className='flex flex-row items-center gap-2'>
+                                            <PencilLine className='h-4 w-4' />
+                                            <h4 className='font-semibold text-gray-700 dark:text-gray-300'>COVER IMAGE</h4>
+                                        </div>
+
+                                        <FileUpload
+                                            handleChange={handleAvatarChange} />
+                                        <Button
+                                            className='w-full'
+                                            type='submit'>
+                                            {
+                                                coverImageLoading ?
+                                                    <Loader2 className='h-4 w-4 animate-spin' />
+                                                    :
+                                                    'Update'
+                                            }
+                                        </Button>
+                                    </div>
+                                </form>
+                            </section>
+                        </main>
+                    </div>
                 </div>
-            </div>
 
-        </section>
-        <ToastContainer 
-        position='top-right'
-        theme='dark'
-        />
+            </section>
+            <ToastContainer
+                position='top-right'
+                theme='dark'
+            />
         </>
 
     )
