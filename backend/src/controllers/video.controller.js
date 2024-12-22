e791549c886ec84bc1ff96fb28bcff95fb453854
Rohit Shrivastav <rohitrks805@gmail.com>
2024-12-22 23:47:37 +0530

    aggregation pipeline | single video fetch

--- backend/src/controllers/video.controller.js
+++ backend/src/controllers/video.controller.js
@@ -5,7 +5,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 
-const uploadVideo = asyncHandler( async(req, res) => {
+const uploadVideo = asyncHandler(async (req, res) => {
     // Algorithm while uploading video 
     // step 1: requirement of title, desc, and views from user (req.boyd)
     // step 2: check for title and description !empty
@@ -19,18 +19,18 @@ const uploadVideo = asyncHandler( async(req, res) => {
     const { title, description, views } = req.body;
     console.log('title server: ', title)
     console.log('desc server: ', description)
-    const  videoLocalPath  = req.files?.videoFile[0]?.path;
-    const  thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+    const videoLocalPath = req.files?.videoFile[0]?.path;
+    const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
 
-    if(!videoLocalPath) throw new ApiError(400, "Invalid url of video")
-    if(!thumbnailLocalPath) throw new ApiError(400, "Invalid url of thumbnail")
+    if (!videoLocalPath) throw new ApiError(400, "Invalid url of video")
+    if (!thumbnailLocalPath) throw new ApiError(400, "Invalid url of thumbnail")
 
     const videoFile = await uploadOnCloudinary(videoLocalPath)
     // TODO: work on thumbnail upload 
     const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
 
-    if(!videoFile) throw new ApiError(400, "Faild to upload video on cloudinary")
-    if(!thumbnail) throw new ApiError(400, "Faild to upload thumbnail on cloudinary")
+    if (!videoFile) throw new ApiError(400, "Faild to upload video on cloudinary")
+    if (!thumbnail) throw new ApiError(400, "Faild to upload thumbnail on cloudinary")
 
     const video = await Video.create(
         {
@@ -47,15 +47,15 @@ const uploadVideo = asyncHandler( async(req, res) => {
     )
 
     return res
-    .status(201)
-    .json(
-        new ApiResponse(200, video, "Video Uploaded successfully")
-    )
+        .status(201)
+        .json(
+            new ApiResponse(200, video, "Video Uploaded successfully")
+        )
 
 })
 
 
-const deleteVideo = asyncHandler( async(req, res) => {
+const deleteVideo = asyncHandler(async (req, res) => {
     // Algorithm while  delete video 
     // step 1: find video id which want to delete 
     // step 2: validation of video id 
@@ -65,20 +65,20 @@ const deleteVideo = asyncHandler( async(req, res) => {
 
     const { videoId } = req.params;
 
-    if(!videoId) throw new ApiError(400, "Invalid video id")
+    if (!videoId) throw new ApiError(400, "Invalid video id")
 
     const deletedVideo = await Video.findByIdAndDelete(videoId)
 
-    if(!deletedVideo) throw new ApiError(400, "something went wrong while deleting the video")
+    if (!deletedVideo) throw new ApiError(400, "something went wrong while deleting the video")
 
     return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Video deleted successfully"))
+        .status(200)
+        .json(new ApiResponse(200, {}, "Video deleted successfully"))
 
 })
 
 
-const updateVideo = asyncHandler( async(req, res) => {
+const updateVideo = asyncHandler(async (req, res) => {
     // Algorithm while update video fields
     // step 1: requirement of content form user like title, description
     // step 2: check for which field want to be update 
@@ -88,49 +88,49 @@ const updateVideo = asyncHandler( async(req, res) => {
     // step 6: check for updated object 
     // step 7: return response 
 
-    const { title, description} = req.body;
+    const { title, description } = req.body;
     const { videoId } = req.params;
 
-    if(!(title && description)) throw new ApiError(400, "title and description are required")
+    if (!(title && description)) throw new ApiError(400, "title and description are required")
 
-    if(!videoId) throw new ApiError(400, "Invalid video id")
- 
+    if (!videoId) throw new ApiError(400, "Invalid video id")
+
     const localThumbnailPath = req.files?.thumbnail[0]?.path;
-    if(!localThumbnailPath) throw new ApiError(400, "Invalid thumbnail url")
+    if (!localThumbnailPath) throw new ApiError(400, "Invalid thumbnail url")
 
     const uploadedThumbnail = await uploadOnCloudinary(localThumbnailPath)
-    if(!uploadedThumbnail) throw new ApiError(400, "something went wrong while updating thumbnail ")
+    if (!uploadedThumbnail) throw new ApiError(400, "something went wrong while updating thumbnail ")
 
     const video = await Video.findById(videoId)
-    if(!video) throw new ApiError(400, "video not found in db")
+    if (!video) throw new ApiError(400, "video not found in db")
 
     const updatedVideoContent = await Video.findByIdAndUpdate(
         videoId,
         {
-           $set: {
-            title,
-            description,
-            thumbnail : uploadedThumbnail.url
-           }
-        }, 
-        {new: true}
+            $set: {
+                title,
+                description,
+                thumbnail: uploadedThumbnail.url
+            }
+        },
+        { new: true }
     )
 
-    if(!updatedVideoContent) throw new ApiError(400, "something went wrong while updating the video content")
+    if (!updatedVideoContent) throw new ApiError(400, "something went wrong while updating the video content")
 
     return res
-    .status(200)
-    .json(
-        new ApiResponse(200, updatedVideoContent, "Video updated successfully ")
-    )
+        .status(200)
+        .json(
+            new ApiResponse(200, updatedVideoContent, "Video updated successfully ")
+        )
 })
 
 
-const getAllVideos = asyncHandler( async(req, res) => {
+const getAllVideos = asyncHandler(async (req, res) => {
     const allVideos = await Video.aggregate(
         [
             {
-                $lookup:{
+                $lookup: {
                     from: "users",
                     localField: "owner",
                     foreignField: "_id",
@@ -141,21 +141,34 @@ const getAllVideos = asyncHandler( async(req, res) => {
     )
 
     return res
-    .status(200)
-    .json( new ApiResponse(200, allVideos, "Videos fetched successfully"))
+        .status(200)
+        .json(new ApiResponse(200, allVideos, "Videos fetched successfully"))
 })
 
 
-const getSingleVideo = asyncHandler(async(req, res)=> {
-    const {id} = req.body;
+const getSingleVideo = asyncHandler(async (req, res) => {
+    const { id } = req.body;
 
     const singleVideo = await Video.findById(id)
 
-    if(!singleVideo) throw new ApiResponse(404, {}, 'video not found' )
-    
+    const creatorInfo = await Video.aggregate(
+        [
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'owner',
+                    foreignField: '_id',
+                    as: 'creatorDetails'
+                }
+            }
+        ]
+    )
+
+    if (!singleVideo) throw new ApiResponse(404, {}, 'video not found')
+
     return res
-    .status(200)
-    .json(new ApiResponse(200, singleVideo, 'video fetched successfully'))
+        .status(200)
+        .json(new ApiResponse(200, {singleVideo, creatorInfo}, 'video fetched successfully'))
 })
 export {
     uploadVideo,
